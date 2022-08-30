16c65060b63cf6753fe9a2cfa9085d2e7c6fedab
Jack Miner <jackminer@pingidentity.com>
2022-08-30 11:36:49 -0600

    [skip ci] - a few qol improvements

--- k8s-configs/cluster-tools/base/pgo/base/update-pgo.sh
+++ k8s-configs/cluster-tools/base/pgo/base/update-pgo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e
 
-if [[ ! "${0}" =~ "k8s-configs/cluster-tools/base/pgo/base" ]]; then
+if [[ ! "$(pwd)" =~ "k8s-configs/cluster-tools/base/pgo/base" ]]; then
     echo "Script run source sanity check failed. Please only run this script in k8s-configs/cluster-tools/base/pgo/base"
+    exit 1
 fi
 
+# TODO: take a SHA to pull a specific version of the example repo
 
 source ../../../../../utils.sh
 
@@ -15,12 +18,15 @@ example_repo="postgres-operator-examples"
 repo_dir="${tmp_dir}/${example_repo}"
 repo_dir_kustomize="${repo_dir}/kustomize/install"
 
-beluga_log "Creating tmp dir - ${tmp_dir}"
+log "Creating tmp dir - ${tmp_dir}"
 mkdir "${tmp_dir}"
 
 git clone "https://github.com/CrunchyData/${example_repo}" "${tmp_dir}/${example_repo}"
 
+# Remove the singlenamespace dir, we don't use it
 rm -rf "${repo_dir_kustomize}/singlenamespace"
 
+# Copy the massaged repo directory to this directory
 rsync -rv "${repo_dir_kustomize}/" .
 
+log "PGO update complete, check your 'git diff' to see what changed"
