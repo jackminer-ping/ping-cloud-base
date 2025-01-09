43745dc5ea6828aa9daf5347ddb5187002de0ac2
Jack Miner <jackminer@pingidentity.com>
2025-01-08 17:41:47 -0700

    [skip ci] Close to working

--- k8s-configs/cluster-tools/base/git-ops/helm-command.sh
+++ k8s-configs/cluster-tools/base/git-ops/helm-command.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+echo "Got args: $@" >> /tmp/jack-helm.txt
+
 if [[ $@ = pull* && $@ = *oci://* ]]; then
+  if [[ $@ = *--repo* ]]; then
     # If the command is `helm pull (..)` skips --repo flag and chartName
     # from command line args to make helm pull run
 
     # For explanation:
     # https://github.com/kubernetes-sigs/kustomize/issues/4381
+
+    #echo "args 1: $@" >> /tmp/jack-args.txt
+    # remove everything up until --repo
     arr=(${@//--repo/});  # Skipping --repo
+    #echo "args 2: ${arr}" >> /tmp/jack-args.txt
+    # get elements 0 through 5, and end to 6, removing all else
     args="${arr[@]:0:5} ${arr[@]:6}";  # Skipping chartName
+    #echo "args 3: ${args}" >> /tmp/jack-args.txt
+  else
+    # Remove from the end of the string up until the first /
+    # This removes the duplicate repo name
+    args="$@"
+    echo "args 1: ${args}" >> /tmp/jack-new-args.txt
+    args[4]="${args[4]%\/*}"
+    echo "args 2: ${args}" >> /tmp/jack-new-args.txt
+  fi
 else
     args="$@"
 fi
